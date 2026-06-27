95f98b9cc1eec5ea2bf86ad7a602404042e03303
iamgamja <hmmthinking@naver.com>
2026-06-27 15:37:12 +0900

    reafctor: render rule

--- src/init/renderRules.ts
+++ src/init/renderRules.ts
@@ -23,7 +23,7 @@ type X = number
 type Y = number
 type Coord = [X, Y]
 
-function calculateCenter(pos1: POSlike, pos2?: POSlike): Coord {
+function calculateCenter(pos1: POSlike, pos2?: POSlike | null): Coord {
   if (!pos2) return [(pos1[1] + 1) * SIZE_CELL + SIZE_CELL / 2, (pos1[0] + 1) * SIZE_CELL + SIZE_CELL / 2]
 
   const [x1, y1] = calculateCenter(pos1)
@@ -99,158 +99,94 @@ function parseDrawLineOptions(options?: DrawLineOptions): ParsedDrawLineOptions 
   }
 }
 
-function createRectangle(w: Y, a: X, s: Y, d: X, { dotted, stroke_color, fill_color, strokeWidth }: ParsedDrawOptions) {
-  const ele = document.createElementNS('http://www.w3.org/2000/svg', 'rect')
-
-  ele.setAttribute('x', a.toString())
-  ele.setAttribute('y', w.toString())
-  ele.setAttribute('width', (d - a).toString())
-  ele.setAttribute('height', (s - w).toString())
-  ele.setAttribute('fill', fill_color)
-  ele.setAttribute('stroke', stroke_color)
-  ele.setAttribute('stroke-width', strokeWidth.toString())
-  if (dotted) ele.setAttribute('stroke-dasharray', '2')
-
-  svg.appendChild(ele)
-}
-
-function createCircle(x: X, y: Y, { dotted, stroke_color, fill_color, strokeWidth, r }: ParsedDrawOptions) {
-  const ele = document.createElementNS('http://www.w3.org/2000/svg', 'circle')
-
-  ele.setAttribute('cx', x.toString())
-  ele.setAttribute('cy', y.toString())
-  ele.setAttribute('r', r.toString())
-  ele.setAttribute('fill', fill_color)
-  ele.setAttribute('stroke', stroke_color)
-  ele.setAttribute('stroke-width', strokeWidth.toString())
-  if (dotted) ele.setAttribute('stroke-dasharray', '2')
-
-  svg.appendChild(ele)
-}
-function createDiamond(x: X, y: Y, { dotted, stroke_color, fill_color, strokeWidth, r }: ParsedDrawOptions) {
-  const ele = document.createElementNS('http://www.w3.org/2000/svg', 'polygon')
-
-  const p1 = `${x},${y - r}` // 상
-  const p2 = `${x + r},${y}` // 우
-  const p3 = `${x},${y + r}` // 하
-  const p4 = `${x - r},${y}` // 좌
-  ele.setAttribute('points', `${p1} ${p2} ${p3} ${p4}`)
-
-  ele.setAttribute('fill', fill_color)
-  ele.setAttribute('stroke', stroke_color)
-  ele.setAttribute('stroke-width', strokeWidth.toString())
-  if (dotted) ele.setAttribute('stroke-dasharray', '2')
-
-  svg.appendChild(ele)
-}
-/**
- * @param direction degree 단위. 오른쪽을 0으로 하고, 반시계방향을 +로 한다.
- */
-function createTriangle(x: X, y: Y, direction: number, { dotted, stroke_color, fill_color, strokeWidth, r }: ParsedDrawOptions) {
-  const ele = document.createElementNS('http://www.w3.org/2000/svg', 'polygon')
-
-  const points = [0, 120, 240].map((a) => {
-    const rad = ((direction + a) * Math.PI) / 180
-    return `${x + r * Math.cos(rad)},${y - r * Math.sin(rad)}`
-  })
-  ele.setAttribute('points', points.join(' '))
-
-  ele.setAttribute('fill', fill_color)
-  ele.setAttribute('stroke', stroke_color)
-  ele.setAttribute('stroke-width', strokeWidth.toString())
-  if (dotted) ele.setAttribute('stroke-dasharray', '2')
-
-  svg.appendChild(ele)
-}
-function createHexagon(x: X, y: Y, { dotted, stroke_color, fill_color, strokeWidth, r }: ParsedDrawOptions) {
-  const ele = document.createElementNS('http://www.w3.org/2000/svg', 'polygon')
-
-  const p1 = `${x},${y - r}` // 상
-  const p2 = `${x + r * 0.866},${y - r * 0.5}` // 우상
-  const p3 = `${x + r * 0.866},${y + r * 0.5}` // 우하
-  const p4 = `${x},${y + r}` // 하
-  const p5 = `${x - r * 0.866},${y + r * 0.5}` // 좌하
-  const p6 = `${x - r * 0.866},${y - r * 0.5}` // 좌상
-  ele.setAttribute('points', `${p1} ${p2} ${p3} ${p4} ${p5} ${p6}`)
-
-  ele.setAttribute('fill', fill_color)
-  ele.setAttribute('stroke', stroke_color)
-  ele.setAttribute('stroke-width', strokeWidth.toString())
-  if (dotted) ele.setAttribute('stroke-dasharray', '2')
-
-  svg.appendChild(ele)
-}
-
-function createLine(x1: X, y1: Y, x2: X, y2: Y, { dotted, color, strokeWidth, round }: ParsedDrawLineOptions) {
-  const ele = document.createElementNS('http://www.w3.org/2000/svg', 'line')
-
-  ele.setAttribute('x1', x1.toString())
-  ele.setAttribute('y1', y1.toString())
-  ele.setAttribute('x2', x2.toString())
-  ele.setAttribute('y2', y2.toString())
-  ele.setAttribute('stroke', color)
-  ele.setAttribute('stroke-width', strokeWidth.toString())
-  if (round) ele.setAttribute('stroke-linecap', 'round')
-  if (dotted) ele.setAttribute('stroke-dasharray', '2')
-
-  svg.appendChild(ele)
-}
-
 const Draw = {
-  Circle(pos: POSlike, options_?: DrawOptions) {
+  _createRectangle(w: Y, a: X, s: Y, d: X, { dotted, stroke_color, fill_color, strokeWidth }: ParsedDrawOptions) {
+    const ele = document.createElementNS('http://www.w3.org/2000/svg', 'rect')
+
+    ele.setAttribute('x', a.toString())
+    ele.setAttribute('y', w.toString())
+    ele.setAttribute('width', (d - a).toString())
+    ele.setAttribute('height', (s - w).toString())
+    ele.setAttribute('fill', fill_color)
+    ele.setAttribute('stroke', stroke_color)
+    ele.setAttribute('stroke-width', strokeWidth.toString())
+    if (dotted) ele.setAttribute('stroke-dasharray', '2')
+
+    svg.appendChild(ele)
+  },
+  /**
+   * @param n 정다각형의 변 개수
+   * @param direction radian 단위. 오른쪽을 0으로 하고, 반시계방향을 +로 한다.
+   */
+  _createPolygon(x: X, y: Y, n: number, direction: number, { dotted, stroke_color, fill_color, strokeWidth, r }: ParsedDrawOptions) {
+    const ele = document.createElementNS('http://www.w3.org/2000/svg', 'polygon')
+
+    const points = Array.from({ length: n }, (_, i) => {
+      const angle = direction + 2 * Math.PI * (i / n)
+
+      return `${x + r * Math.cos(angle)},${y - r * Math.sin(angle)}`
+    })
+
+    ele.setAttribute('points', points.join(' '))
+
+    ele.setAttribute('fill', fill_color)
+    ele.setAttribute('stroke', stroke_color)
+    ele.setAttribute('stroke-width', strokeWidth.toString())
+    if (dotted) ele.setAttribute('stroke-dasharray', '2')
+
+    svg.appendChild(ele)
+  },
+  _createLine(x1: X, y1: Y, x2: X, y2: Y, { dotted, color, strokeWidth, round }: ParsedDrawLineOptions) {
+    const ele = document.createElementNS('http://www.w3.org/2000/svg', 'line')
+
+    ele.setAttribute('x1', x1.toString())
+    ele.setAttribute('y1', y1.toString())
+    ele.setAttribute('x2', x2.toString())
+    ele.setAttribute('y2', y2.toString())
+    ele.setAttribute('stroke', color)
+    ele.setAttribute('stroke-width', strokeWidth.toString())
+    if (round) ele.setAttribute('stroke-linecap', 'round')
+    if (dotted) ele.setAttribute('stroke-dasharray', '2')
+
+    svg.appendChild(ele)
+  },
+
+  Circle(pos1: POSlike, pos2?: POSlike | null, options_?: DrawOptions) {
     const options: DrawOptions = {
-      size: 'regular',
+      size: !pos2 ? 'regular' : 'smallest',
+      ...options_,
+    }
+    const [x, y] = calculateCenter(pos1, pos2)
+
+    Draw._createPolygon(x, y, 50, 0, parseDrawOptions(options))
+  },
+  Diamond(pos: POSlike, pos2?: POSlike | null, options_?: DrawOptions) {
+    const options: DrawOptions = {
+      size: !pos2 ? 'regular' : 'smallest',
       ...options_,
     }
     const [x, y] = calculateCenter(pos)
 
-    createCircle(x, y, parseDrawOptions(options))
+    Draw._createPolygon(x, y, 4, 0, parseDrawOptions(options))
   },
-  MidCircle(pos1: POSlike, pos2: POSlike, options_?: DrawOptions) {
+  Triangle(pos1: POSlike, pos2?: POSlike | null, options_?: DrawOptions) {
     const options: DrawOptions = {
-      size: 'smallest',
+      size: !pos2 ? 'regular' : 'small',
       ...options_,
     }
     const [x, y] = calculateCenter(pos1, pos2)
 
-    createCircle(x, y, parseDrawOptions(options))
+    const direction = !pos2 ? 0 : Math.atan2(-(pos2[0] - pos1[0]), pos2[1] - pos1[1])
+    Draw._createPolygon(x, y, 3, direction, parseDrawOptions(options))
   },
-  Diamond(pos: POSlike, options_?: DrawOptions) {
+  Hexagon(pos1: POSlike, pos2?: POSlike | null, options_?: DrawOptions) {
     const options: DrawOptions = {
-      size: 'regular',
-      ...options_,
-    }
-    const [x, y] = calculateCenter(pos)
-
-    createDiamond(x, y, parseDrawOptions(options))
-  },
-  MidDiamond(pos1: POSlike, pos2: POSlike, options_?: DrawOptions) {
-    const options: DrawOptions = {
-      size: 'smallest',
+      size: !pos2 ? 'regular' : 'small',
       ...options_,
     }
     const [x, y] = calculateCenter(pos1, pos2)
 
-    createDiamond(x, y, parseDrawOptions(options))
-  },
-  MidTriangle(pos1: POSlike, pos2: POSlike, options_?: DrawOptions) {
-    const options: DrawOptions = {
-      size: 'small',
-      ...options_,
-    }
-    const [x, y] = calculateCenter(pos1, pos2)
-    const direction = (Math.atan2(-(pos2[0] - pos1[0]), pos2[1] - pos1[1]) * 180) / Math.PI
-
-    createTriangle(x, y, direction, parseDrawOptions(options))
-  },
-  MidHexagon(pos1: POSlike, pos2: POSlike, options_?: DrawOptions) {
-    const options: DrawOptions = {
-      size: 'small',
-      ...options_,
-    }
-    const [x, y] = calculateCenter(pos1, pos2)
-
-    createHexagon(x, y, parseDrawOptions(options))
+    Draw._createPolygon(x, y, 6, 0, parseDrawOptions(options))
   },
 
   Divider(pos1: POSlike, pos2: POSlike, options?: DrawLineOptions) {
@@ -263,13 +199,13 @@ const Draw = {
     const newX2 = cx - (y2 - cy)
     const newY2 = cy + (x2 - cx)
 
-    createLine(newX1, newY1, newX2, newY2, parseDrawLineOptions(options))
+    Draw._createLine(newX1, newY1, newX2, newY2, parseDrawLineOptions(options))
   },
   Line(pos1: POSlike, pos2: POSlike, options?: DrawLineOptions) {
     const [x1, y1] = calculateCenter(pos1)
     const [x2, y2] = calculateCenter(pos2)
 
-    createLine(x1, y1, x2, y2, parseDrawLineOptions(options))
+    Draw._createLine(x1, y1, x2, y2, parseDrawLineOptions(options))
   },
   Cage(group: Group, options_?: DrawOptions) {
     const parsedRectOptions = parseDrawOptions({
@@ -315,35 +251,35 @@ const Draw = {
       const d = x + r
 
       // fill
-      createRectangle(w, a, s, d, parsedRectOptions)
-      if (has_map.W) createRectangle(W, a, w, d, parsedRectOptions)
-      if (has_map.A) createRectangle(w, A, s, a, parsedRectOptions)
-      if (has_map.X) createRectangle(s, a, S, d, parsedRectOptions)
-      if (has_map.D) createRectangle(w, d, s, D, parsedRectOptions)
+      Draw._createRectangle(w, a, s, d, parsedRectOptions)
+      if (has_map.W) Draw._createRectangle(W, a, w, d, parsedRectOptions)
+      if (has_map.A) Draw._createRectangle(w, A, s, a, parsedRectOptions)
+      if (has_map.X) Draw._createRectangle(s, a, S, d, parsedRectOptions)
+      if (has_map.D) Draw._createRectangle(w, d, s, D, parsedRectOptions)
 
-      if (has_map.Q && has_map.W && has_map.A) createRectangle(W, A, w, a, parsedRectOptions)
-      if (has_map.E && has_map.W && has_map.D) createRectangle(W, d, w, D, parsedRectOptions)
+      if (has_map.Q && has_map.W && has_map.A) Draw._createRectangle(W, A, w, a, parsedRectOptions)
+      if (has_map.E && has_map.W && has_map.D) Draw._createRectangle(W, d, w, D, parsedRectOptions)
 
-      if (has_map.Z && has_map.X && has_map.A) createRectangle(s, A, S, a, parsedRectOptions)
-      if (has_map.C && has_map.X && has_map.D) createRectangle(s, d, S, D, parsedRectOptions)
+      if (has_map.Z && has_map.X && has_map.A) Draw._createRectangle(s, A, S, a, parsedRectOptions)
+      if (has_map.C && has_map.X && has_map.D) Draw._createRectangle(s, d, S, D, parsedRectOptions)
 
       // stroke
-      if (!has_map.W) createLine(a, w, d, w, parsedLineOptions)
-      if (!has_map.A) createLine(a, w, a, s, parsedLineOptions)
-      if (!has_map.X) createLine(a, s, d, s, parsedLineOptions)
-      if (!has_map.D) createLine(d, w, d, s, parsedLineOptions)
+      if (!has_map.W) Draw._createLine(a, w, d, w, parsedLineOptions)
+      if (!has_map.A) Draw._createLine(a, w, a, s, parsedLineOptions)
+      if (!has_map.X) Draw._createLine(a, s, d, s, parsedLineOptions)
+      if (!has_map.D) Draw._createLine(d, w, d, s, parsedLineOptions)
 
-      if (has_map.A && !(has_map.Q && has_map.W)) createLine(A, w, a, w, parsedLineOptions)
-      if (has_map.A && !(has_map.Z && has_map.X)) createLine(A, s, a, s, parsedLineOptions)
+      if (has_map.A && !(has_map.Q && has_map.W)) Draw._createLine(A, w, a, w, parsedLineOptions)
+      if (has_map.A && !(has_map.Z && has_map.X)) Draw._createLine(A, s, a, s, parsedLineOptions)
 
-      if (has_map.W && !(has_map.Q && has_map.A)) createLine(a, W, a, w, parsedLineOptions)
-      if (has_map.W && !(has_map.E && has_map.D)) createLine(d, W, d, w, parsedLineOptions)
+      if (has_map.W && !(has_map.Q && has_map.A)) Draw._createLine(a, W, a, w, parsedLineOptions)
+      if (has_map.W && !(has_map.E && has_map.D)) Draw._createLine(d, W, d, w, parsedLineOptions)
 
-      if (has_map.X && !(has_map.Z && has_map.A)) createLine(a, s, a, S, parsedLineOptions)
-      if (has_map.X && !(has_map.C && has_map.D)) createLine(d, s, d, S, parsedLineOptions)
+      if (has_map.X && !(has_map.Z && has_map.A)) Draw._createLine(a, s, a, S, parsedLineOptions)
+      if (has_map.X && !(has_map.C && has_map.D)) Draw._createLine(d, s, d, S, parsedLineOptions)
 
-      if (has_map.D && !(has_map.E && has_map.W)) createLine(d, w, D, w, parsedLineOptions)
-      if (has_map.D && !(has_map.C && has_map.X)) createLine(d, s, D, s, parsedLineOptions)
+      if (has_map.D && !(has_map.E && has_map.W)) Draw._createLine(d, w, D, w, parsedLineOptions)
+      if (has_map.D && !(has_map.C && has_map.X)) Draw._createLine(d, s, D, s, parsedLineOptions)
     }
   },
 }
@@ -352,11 +288,6 @@ const Draw = {
 
 /**
 @todo 렌더링 아이디어
-
-- MR
-Draw.Line([0, 0], [1, 1], { color: '#fe4b196b', thickness: 'hint_light' })
-Draw.Line([0, 2], [1, 1], { color: '#fe4b196b', thickness: 'hint_light' })
-Draw.Line([0, 2], [1, 2], { color: '#fe4b196b', thickness: 'hint_light' })
 
 - RF
 Draw.Line([5, 0 - 0.5], [5, 8 + 0.5], { color: '#fe4b196b', thickness: 'hint_regular', round: false })
@@ -448,7 +379,7 @@ function render_rule(rule: Rule): boolean {
     case '[LK]':
     case "[LK']": {
       rule.render_state.edges.forEach(([pos1, pos2]) => {
-        Draw.MidDiamond(pos1, pos2)
+        Draw.Diamond(pos1, pos2)
       })
       return true
     }
@@ -472,34 +403,34 @@ function render_rule(rule: Rule): boolean {
 
     case '[LO]': {
       rule.render_state.cells.forEach((pos) => {
-        Draw.Circle(pos, { stroke_color: '#b9ff49', fill_color: '#b9ff4954' })
+        Draw.Circle(pos, null, { stroke_color: '#b9ff49', fill_color: '#b9ff4954' })
       })
       return true
     }
     case "[LO']": {
       rule.render_state.cells.forEach((pos) => {
-        Draw.Circle(pos, { stroke_color: '#49f9ff', fill_color: '#49f9ff54' })
+        Draw.Circle(pos, null, { stroke_color: '#49f9ff', fill_color: '#49f9ff54' })
       })
       return true
     }
 
     case '[PO]': {
       rule.render_state.edges.forEach(([pos1, pos2]) => {
-        Draw.MidTriangle(pos1, pos2, { stroke_color: '#ffffff', fill_color: '#000000' })
+        Draw.Triangle(pos1, pos2, { stroke_color: '#ffffff', fill_color: '#000000' })
       })
       return true
     }
 
     case '[PR]': {
       rule.render_state.edges.forEach(([r1, c1, r2, c2, isred]) => {
-        Draw.MidHexagon([r1, c1], [r2, c2], { stroke_color: '#ffffff', fill_color: isred ? '#ff0000cc' : '#0000ffcc' })
+        Draw.Hexagon([r1, c1], [r2, c2], { stroke_color: '#ffffff', fill_color: isred ? '#ff0000cc' : '#0000ffcc' })
       })
       return true
     }
     case "[PR']": {
       rule.render_state.triplets.forEach(([r1, c1, r2, c2, r3, c3, isred]) => {
-        Draw.MidHexagon([r1, c1], [r2, c2], { stroke_color: '#ffffff', fill_color: isred ? '#ff0000cc' : '#0000ffcc' })
-        Draw.MidHexagon([r2, c2], [r3, c3], { stroke_color: '#ffffff', fill_color: isred ? '#ff0000cc' : '#0000ffcc' })
+        Draw.Hexagon([r1, c1], [r2, c2], { stroke_color: '#ffffff', fill_color: isred ? '#ff0000cc' : '#0000ffcc' })
+        Draw.Hexagon([r2, c2], [r3, c3], { stroke_color: '#ffffff', fill_color: isred ? '#ff0000cc' : '#0000ffcc' })
       })
       return true
     }
