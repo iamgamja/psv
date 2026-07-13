da0e2d151c3cee5c3cfc48149d80074decd1bbb4
iamgamja <hmmthinking@naver.com>
2026-07-13 16:30:11 +0900

    fix: [DT]

--- src/const/groups.ts
+++ src/const/groups.ts
@@ -14,7 +14,7 @@ for (const r of IDX0) for (const c of IDX0) GROUPS_C[c].push([r, c])
 const GROUPS_B: Groups = Array.from({ length: 9 }, () => [])
 for (const r of IDX0) for (const c of IDX0) GROUPS_B[Math.floor(r / 3) * 3 + Math.floor(c / 3)].push([r, c])
 
-const GROUPS_DT: TwoGroups = GROUPS_ADJACENT['wasd']
+const GROUPS_DT: TwoGroups = GROUPS_ADJACENT['king']
 
 export const GROUPS_QD: Groups = []
 for (let r = 0; r < 8; r++) {
